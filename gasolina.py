e5735a5060b331789d424b79ce3a6b5f8c9ab784
Rafaabreu1 <abreurafa1@hotmail.com>
2025-01-06 03:41:32 +0000

    Adicionando o arquivo gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -1,18 +1,5 @@
- import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
-
-%%writefile gasolina.csv
-dia,venda
-1,5.11
-2,4.99
-3,5.02
-4,5.21
-5,5.07
-6,5.09
-7,5.13
-8,5.12
-9,4.94
-10,5.03
 
 df = pd.read_csv("gasolina.csv", sep=",")
 plt.plot(df["dia"], df["venda"])
