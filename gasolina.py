4c3f0dafc29bcc643786e7f5a1c8e75a69863638
Rafaabreu1 <abreurafa1@hotmail.com>
2025-01-06 16:26:44 +0000

    Adicionando tabela da gasolina

--- gasolina.py
+++ gasolina.py
@@ -1,9 +1,1 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("gasolina.csv", sep=",")
-plt.plot(df["dia"], df["venda"])
-plt.xlabel("Dia")
-plt.ylabel("Preço")
-plt.title("Preço da gasolina")
-plt.savefig("gasolina.png")
+ 
